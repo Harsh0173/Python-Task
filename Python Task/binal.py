21c2d271c713d1e6d55fac1fe44b2d8444d72afa
Harsh0173 <pythonsharinggithub@gmail.com>
2024-05-16 16:07:14 +0530

    bra desine upload

--- Python Task/binal.py
+++ Python Task/binal.py
@@ -3,10 +3,11 @@ n= int(input("enter value :"))
 if n<5 or n>50 :
     print('please enter value up to 5 and less to 50')
 else :
-    for i in range(0,n+1):
-        for j in range(i):
-            print ("*" ,end="")
-        print(" ")
-        
-        
-
+       for i in range(1,n+1):
+            for m in range(i):
+                    print("*",end="")
+            for j in range(2*n-i*2):
+                    print(" ",end="")
+            for k in range(0,i):
+                    print("*",end="")
+            print()
