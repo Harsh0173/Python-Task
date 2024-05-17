868bdc1e85a53c26f54c3eb831995d11a7c2a0fb
Harsh0173 <pythonsharinggithub@gmail.com>
2024-05-17 15:09:55 +0530

    Update array.py

--- Python Task/array.py
+++ Python Task/array.py
@@ -1,2 +1,15 @@
-a=list(input("Enter your array mumber:-"));
-for i in list(a):
+n= int(input("enter value :"))
+
+if n<5 or n>20 :
+    print('please enter value up to 5 and less to 20')
+else :
+        for i in range(n):
+                print("*",end="")
+        print()
+        for k in range(1,n):
+            print("*",end="")
+            for j in range(2,n):
+                print(" ",end="")
+            print("*")
+        for m in range(n):
+                print("*",end="")
